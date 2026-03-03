c3dbc2b00290b0e080d798c6dd58febd1aa5b509
tanishgoel957-crypto <tanishgoel957@gmail.com>
2026-03-02 21:50:11 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+import discord
+import os
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+client.run(os.getenv("TOKEN"))
